2f56bc9d1ec72d29c094048fd6d3a405b5df94ef
Tom <caminade.tom@mail.com>
2019-01-31 11:47:11 +0100

    Def des salles par enum

---
+++ src/NF/Type_Salle.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NF;
+
+/**
+ *
+ * @author camin
+ */
+public enum Type_Salle {
+    SALLE_1("Scanner",true ),
+    SALLE_2("Scanner",true),
+    SALLE_3("Angiographe",true),
+    SALLE_4("IRM",true),
+    SALLE_5("Radio Argentique",false),
+    SALLE_6("Radio Argentique",false),
+    SALLE_7("Radio Argentique",false),
+    SALLE_8("Radiothérapie";true);
+    
+    private String Equipement_Salle;
+    private boolean Salle_num;
+    Type_salle(String Equipement_Salle, boolean Salle_num){
+        this.Equipement_Salle=Equipement_Salle;
+        this.Salle_num=Salle_num;                      
+    }
+    
+    
+    
+    
+    
+}
