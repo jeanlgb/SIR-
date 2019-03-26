308686447410f88a0e572801b6bbf07b86bd1e45
Maud <maud.leclerctracy@gmail.com>
2019-03-26 22:17:31 +0100

    login anglais

--- src/UI/Login.java
+++ src/UI/Login.java
@@ -305,6 +305,7 @@ public class Login extends javax.swing.JFrame {
      * Méthode pour changer de langue renvoyant au login
      */
     private void jButtonChangerLangueActionPerformed(java.awt.event.ActionEvent evt) {
+        //UI_Anglais.Login;
         // TODO add your handling code here:
     }
 
@@ -405,6 +406,7 @@ public class Login extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+        
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
